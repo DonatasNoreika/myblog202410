cc7b9eee7f05fc3a407ef779116125cdd1a83067
DonatasNoreika <donoras@gmail.com>
2024-10-18 10:27:55 +0300

    + Galimybė palikti komentarą prisijungusiam vartotojui prie posto

--- mysite/myblog/views.py
+++ mysite/myblog/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import reverse
 from django.views import generic
-
+from django.views.generic.edit import FormMixin
+from .forms import CommentForm
 from .models import Post
 
 
@@ -13,7 +14,25 @@ class PostListView(generic.ListView):
     paginate_by = 5
 
 
-class PostDetailView(generic.DetailView):
+class PostDetailView(FormMixin, generic.DetailView):
     model = Post
     template_name = "post.html"
     context_object_name = "post"
+    form_class = CommentForm
+
+    def get_success_url(self):
+        return reverse("post", kwargs={"pk": self.object.id})
+
+    def form_valid(self, form):
+        form.instance.post = self.object
+        form.instance.user = self.request.user
+        form.save()
+        return super().form_valid(form)
+
+    def post(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        form = self.get_form()
+        if form.is_valid():
+            return self.form_valid(form)
+        else:
+            return self.form_invalid(form)
