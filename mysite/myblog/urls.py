35b79c904db5fc5eecede4d3fda75ed367f15595
DonatasNoreika <donoras@gmail.com>
2024-10-18 09:11:58 +0300

    + Įrašų sąrašo puslapis

--- mysite/myblog/urls.py
+++ mysite/myblog/urls.py
@@ -2,6 +2,8 @@
 from django.contrib import admin
 from django.urls import path
 
-urlpatterns = [
+from . import views
 
+urlpatterns = [
+    path("", views.PostListView.as_view(), name="posts"),
 ]
