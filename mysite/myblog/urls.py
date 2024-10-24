9e57626f297d31693d7d3d14e10f296337906f57
DonatasNoreika <donoras@gmail.com>
2024-10-24 08:50:06 +0300

    + Padaryti, kad vartotojai galėtų patys registruotis

--- mysite/myblog/urls.py
+++ mysite/myblog/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path("register/", views.register, name='register'),
     path("", views.PostListView.as_view(), name="posts"),
     path("posts/<int:pk>", views.PostDetailView.as_view(), name="post"),
     path("userposts/", views.UserPostListView.as_view(), name="userposts"),
